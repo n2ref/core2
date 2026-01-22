82c517f90d2da997af8fe32e11fe81bd35d40911
shabuninil <shabuninil24@gmail.com>
2026-01-22 16:43:51 +0300

    Исправление генерации документации апи

--- inc/classes/OpenApiSpec.php
+++ inc/classes/OpenApiSpec.php
@@ -15,6 +15,7 @@ class OpenApiSpec extends Acl {
     public function getSections(): array {
 
         $sections = [
+            [ 'name' => 'all', 'title' => 'Все', ],
             [ 'name' => 'core2', 'title' => 'Core2', ]
         ];
 
@@ -37,18 +38,19 @@ class OpenApiSpec extends Acl {
                     ];
 
                 } else {
-                    require_once $controller_path;
-                    $openapi        = \OpenApi\Generator::scan([$controller_path], ['exclude' => ['vendor'], 'pattern' => '*.php']);
-                    $section_scheme = ($openapi)->toJson();
+                    if ($this->issetSwaggerAnnotationsInFile($controller_path)) {
+                        $openapi        = \OpenApi\Generator::scan([$controller_path], ['exclude' => ['vendor'], 'pattern' => '*.php']);
+                        $section_scheme = $openapi->toJson();
 
-                    if ( ! empty($section_scheme)) {
-                        $section_scheme = json_decode($section_scheme, true);
+                        if ( ! empty($section_scheme)) {
+                            $section_scheme = json_decode($section_scheme, true);
 
-                        if (count($section_scheme) > 1) {
-                            $sections[$mod['module_id']] = [
-                                'name'  => $mod['module_id'],
-                                'title' => trim(strip_tags($mod['m_name']))
-                            ];
+                            if (count($section_scheme) > 1) {
+                                $sections[$mod['module_id']] = [
+                                    'name'  => $mod['module_id'],
+                                    'title' => trim(strip_tags($mod['m_name']))
+                                ];
+                            }
                         }
                     }
                 }
@@ -68,30 +70,17 @@ class OpenApiSpec extends Acl {
     public function getSectionSchema(string $section): array {
 
         if ($section == 'core2') {
-            $schema_content = file_get_contents(__DIR__ . '/../../schema.json');
-            $section_schema = json_decode($schema_content, true);
+            $section_schema = $this->getSchemeCore2();
+
+        } elseif ($section == 'all') {
+            $section_schema = $this->getSchemeAll();
 
         } else {
             $mods = $this->dataModules->getModuleList();
 
             foreach ($mods as $mod) {
                 if ($mod['module_id'] == $section) {
-
-                    $location    = $this->getModuleLocation($mod['module_id']);
-                    $file_schema = "{$location}/Api/schema.json";
-
-                    if (file_exists($file_schema)) {
-                        $schema_content = file_get_contents($file_schema);
-                        $section_schema = json_decode($schema_content, true);
-
-                    } else {
-                        $controller      = "Mod" . ucfirst(strtolower($mod['module_id'])) . "Api";
-                        $controller_path = "{$location}/{$controller}.php";
-
-                        if (file_exists($controller_path)) {
-                            $section_schema = (\OpenApi\Generator::scan([$controller_path]))->toJson();
-                        }
-                    }
+                    $section_schema = $this->getSchemeModule($mod['module_id']);
                     break;
                 }
             }
@@ -121,5 +110,154 @@ class OpenApiSpec extends Acl {
         }
 
         return [];
+    }
+
+
+    /**
+     * @return array
+     */
+    private function getSchemeCore2(): array {
+
+        $schema_content = file_get_contents(__DIR__ . '/../../schema.json');
+        return json_decode($schema_content, true);
+    }
+
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    private function getSchemeAll(): array {
+
+        $sections = [];
+        $sections[] = ['title' => 'Core2', 'scheme' => $this->getSchemeCore2()];
+
+        $mods = $this->dataModules->getModuleList();
+
+        foreach ($mods as $mod) {
+            $scheme_module = $this->getSchemeModule($mod['module_id']);
+
+            if ($scheme_module) {
+                $sections[] = ['title' => $mod['m_name'], 'scheme' => $scheme_module];
+            }
+        }
+
+
+        $result = $sections[0]['scheme'];
+        $result['info']['title'] = $this->config->system->name;
+
+        foreach ($sections as $section) {
+
+            $scheme        = $section['scheme'];
+            $section_title = trim(strip_tags($section['title']));
+
+            if ( ! empty($scheme['components']) && ! empty($scheme['components']['schemas'])) {
+                foreach ($scheme['components']['schemas'] as $component_name => $component_schema) {
+
+                    if (empty($result['components']['schemas'][$component_name])) {
+                        $result['components']['schemas'][$component_name] = $component_schema;
+                    }
+                }
+            }
+
+            if ( ! empty($scheme['paths'])) {
+                foreach ($scheme['paths'] as $path => $path_schema) {
+
+                    if (empty($result['paths'][$path])) {
+
+                        foreach ($path_schema as $http_method => $method_scheme) {
+                            $tags = ! empty($method_scheme['tags']) ? $method_scheme['tags'] : [];
+
+                            if ( ! empty($tags[0])) {
+                                if ( ! str_starts_with($tags[0], $section_title)) {
+                                    $tags[0] = "{$section_title} - {$tags[0]}";
+                                }
+
+                            } else {
+                                $tags[0] = $section_title;
+                            }
+
+                            $path_schema[$http_method]['tags'] = $tags;
+                        }
+
+                        $result['paths'][$path] = $path_schema;
+                    }
+                }
+            }
+        }
+
+        return $result;
+    }
+
+
+    /**
+     * @param string $module_name
+     * @return array
+     * @throws \Exception
+     */
+    private function getSchemeModule(string $module_name): array {
+
+        $location    = $this->getModuleLocation($module_name);
+        $file_schema = "{$location}/Api/schema.json";
+
+        if (file_exists($file_schema)) {
+            $schema_content = file_get_contents($file_schema);
+            $section_schema = json_decode($schema_content, true);
+
+        } else {
+            $controller      = "Mod" . ucfirst(strtolower($module_name)) . "Api";
+            $controller_path = "{$location}/{$controller}.php";
+
+            if (file_exists($controller_path) && $this->issetSwaggerAnnotationsInFile($controller_path)) {
+                $section_schema = (\OpenApi\Generator::scan([$controller_path]))->toJson();
+            }
+        }
+
+        return ! empty($section_schema)
+            ? $section_schema
+            : [];
+    }
+
+
+    /**
+     * @param array $schemes
+     * @return array
+     */
+    private function mergeSchemes(array $schemes): array {
+
+    }
+
+
+    /**
+     * @param string $filePath
+     * @return bool
+     */
+    private function issetSwaggerAnnotationsInFile(string $filePath): bool {
+
+        try {
+            $context  = new \OpenApi\Context(['filename' => $filePath]);
+            $analysis = new \OpenApi\Analysis([], $context);
+            $analyser = new \OpenApi\Analysers\ReflectionAnalyser();
+
+            $analysis->addAnalysis($analyser->fromFile($filePath, $context));
+
+            $operations = $analysis->getAnnotationsOfType([
+                \OpenApi\Annotations\Get::class,
+                \OpenApi\Annotations\Post::class,
+                \OpenApi\Annotations\Put::class,
+                \OpenApi\Annotations\Delete::class,
+                \OpenApi\Annotations\Patch::class,
+                \OpenApi\Annotations\Options::class,
+                \OpenApi\Annotations\Head::class,
+                \OpenApi\Annotations\Trace::class,
+            ]);
+
+            return count($operations) > 0;
+
+        } catch (\Exception $e) {
+            // ignore
+        }
+
+        return false;
     }
 }
