c4cee54b94169fda4468c81b2c84733c8eb6495c
shabuninil <shabuninil24@gmail.com>
2026-05-21 12:40:48 +0300

    Добавлены более точные ошибки в роутере

--- inc/classes/Routing/Route.php
+++ inc/classes/Routing/Route.php
@@ -125,24 +125,18 @@ class Route {
      */
     private function validateAction(array|\Closure $callback): void {
 
-        if (is_array($callback)) {
-            $correct = ! empty($callback[0]) &&
-                       ! empty($callback[1]) &&
-                       is_string($callback[1]);
-
-            if ($correct) {
-                if (is_string($callback[0])) {
-                    if ( ! class_exists($callback[0])) {
-                        $correct = false;
-                    }
-
-                } elseif ( ! is_object($callback[0])) {
-                    $correct = false;
+        if (is_array($callback) &&
+            ! empty($callback[0]) &&
+            ! empty($callback[1]) &&
+            is_string($callback[1])
+        ) {
+            if (is_string($callback[0])) {
+                if ( ! class_exists($callback[0])) {
+                    throw new \Exception('Class api not found: ' . $callback[0]);
                 }
-            }
 
-            if ( ! $correct) {
-                throw new \Exception('Error callback param');
+            } elseif ( ! is_object($callback[0])) {
+                throw new \Exception('Action api incorrect. Check router params');
             }
         }
     }
